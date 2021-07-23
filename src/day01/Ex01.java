374952af4770a9fc4a3eafc89c61773ac136e9ca
leenayoung <lny950@naver.com>
2021-07-23 11:19:10 +0900

    주석 방법

--- src/day01/Ex01.java
+++ src/day01/Ex01.java
@@ -2,9 +2,14 @@ package day01;
 
 public class Ex01 {
 	public static void main(String[] args) {
+		// sysout 는 괄호안의 내용을 출력하는 기능
+		// ; (세미콜론) 는 문장의 끝을 알리는 용도
+		/*
 		System.out.println("안녕하세요");
 		System.out.println("자바 시간입니다");
-		
+		*/
+		System.out.print("안녕하세요");
+		System.out.print("자바 시간입니다");
 		
 	}
 }
